c8b1a06c6801365103cc12046f7be90f99c67f93
Salman <nazeersalman978@gmail.com>
2024-09-05 00:18:57 +0500

    bottom nav added

---
+++ app/src/main/java/com/sur_tec/helmetiq/MainActivity.kt
@@ -0,0 +1,57 @@
+package com.sur_tec.helmetiq
+
+import HelmetIQNavigation
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.core.view.WindowCompat
+import com.sur_tec.helmetiq.ui.theme.HelmetIQTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+
+        setContent {
+            HelmetIQApp {
+                HelmetIQNavigation()
+            }
+        }
+    }
+}
+
+
+@Composable
+fun HelmetIQApp(content: @Composable () -> Unit) {
+    HelmetIQTheme {
+        Surface(
+            modifier = Modifier.fillMaxSize()
+        ) {
+
+            content()
+        }
+    }
+}
+
+/*  @Composable
+  fun Greeting(name: String, modifier: Modifier = Modifier) {
+      Text(
+          text = "Hello $name!",
+          modifier = modifier
+      )
+  }
+
+  @Preview(showBackground = true)
+  @Composable
+  fun GreetingPreview() {
+      HelmetIQTheme {
+          Greeting("Android")
+      }
+  }
+}
+
+ */
